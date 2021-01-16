eb1a48798dee87ead9c6e1b1f732ca22056aee36
Grant Holley <gvholley@gmail.com>
2021-01-16 21:22:48 +0900

    Cleaned up layout

--- src/CardList.js
+++ src/CardList.js
@@ -4,7 +4,12 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
   const cardComponent = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[0].name} email={robots[0].email} />
+    return <Card
+    key={i}
+    id={robots[i].id}
+    name={robots[0].name}
+    email={robots[0].email}
+    />
   })
   return (
     <div>
