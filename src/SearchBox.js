880309be77b04c8388d589657fee64d0adca105c
Grant Holley <gvholley@gmail.com>
2021-01-20 20:03:13 +0900

    Added SearchBox

---
+++ src/SearchBox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBox = () => {
+
+  return (
+    <div className="pa3 ba ba--green bg-lightest-blue">
+      <input
+      type='search'
+      placeholder='Search!!!!'
+      />
+    </div>
+  );
+}
+
+export default SearchBox;
